8678bbf92658becf2534acabd76ad52e3db9b232
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2022-06-22 22:12:31 +0300

    Handle stale-if-error

--- lib/tesla_http_cache.ex
+++ lib/tesla_http_cache.ex
@@ -6,6 +6,7 @@ defmodule TeslaHTTPCache do
   @behaviour Tesla.Middleware
 
   @default_opts [auto_accept_encoding: true, auto_compress: true, type: :private]
+  @stale_if_error_status [500, 502, 503, 504]
 
   defmodule InvalidBodyError do
     defexception [:message]
@@ -44,16 +45,11 @@ defmodule TeslaHTTPCache do
       :miss ->
         :telemetry.execute([:tesla_http_cache, :miss], %{})
 
-        request_time = now()
+        opts = Keyword.put(opts, :request_time, now())
 
-        case Tesla.run(env, next) do
-          {:ok, env} ->
-            {:ok,
-             cache_new_response(request, env, Keyword.put(opts, :request_time, request_time))}
-
-          {:error, _} = error ->
-            handle_request_failure(request, error, env, opts)
-        end
+        env
+        |> Tesla.run(next)
+        |> handle_response(request, env, opts)
     end
   rescue
     e in InvalidBodyError ->
@@ -63,25 +59,6 @@ defmodule TeslaHTTPCache do
       Tesla.run(env, next)
   end
 
-  defp handle_request_failure(request, {:error, reason} = error, env, opts) do
-    if origin_unreachable?(reason) do
-      opts = Keyword.put(opts, :origin_unreachable, true)
-
-      case :http_cache.get(request, opts) do
-        {:fresh, _} = response ->
-          return_cached_response(response, env, opts)
-
-        {:stale, _} = response ->
-          return_cached_response(response, env, opts)
-
-        _ ->
-          error
-      end
-    else
-      error
-    end
-  end
-
   defp revalidate(
          request,
          {_, {_, {_, cached_headers, _} = revalidated_response}},
@@ -89,31 +66,100 @@ defmodule TeslaHTTPCache do
          next,
          opts
        ) do
-    request_time = now()
+    opts = Keyword.put(opts, :request_time, now())
 
     env
     |> add_validator(cached_headers, "last-modified", "if-modified-since")
     |> add_validator(cached_headers, "etag", "if-none-match")
     |> Tesla.run(next)
-    |> case do
-      {:ok, %Tesla.Env{status: 304} = env} ->
-        :telemetry.execute([:tesla_http_cache, :hit], %{}, %{freshness: :revalidated})
+    |> handle_response(request, revalidated_response, env, opts)
+  end
 
-        {:ok,
-         cache_revalidated_response(
-           request,
-           env,
-           revalidated_response,
-           Keyword.put(opts, :request_time, request_time)
-         )}
+  defp handle_response(result, http_cache_req, http_cache_revalidated_resp \\ nil, req_env, opts)
 
-      {:ok, env} ->
-        :telemetry.execute([:tesla_http_cache, :miss], %{})
+  defp handle_response(
+         {:ok, %Tesla.Env{status: status} = resp_env},
+         http_cache_req,
+         _http_cache_revalidated_resp,
+         _req_env,
+         opts
+       )
+       when status in @stale_if_error_status do
+    opts = Keyword.put(opts, :allow_stale_if_error, true)
 
-        {:ok, cache_new_response(request, env, opts)}
+    case :http_cache.get(http_cache_req, opts) do
+      {:fresh, _} = http_cache_resp ->
+        return_cached_response(http_cache_resp, resp_env, opts)
 
-      {:error, _} = error ->
-        handle_request_failure(request, error, env, opts)
+      {:stale, _} = http_cache_resp ->
+        return_cached_response(http_cache_resp, resp_env, opts)
+
+      _ ->
+        {:ok, resp_env}
+    end
+  end
+
+  defp handle_response(
+         {:ok, %Tesla.Env{status: 304} = resp_env},
+         http_cache_req,
+         http_cache_revalidated_resp,
+         _req_env,
+         opts
+       ) do
+    :telemetry.execute([:tesla_http_cache, :hit], %{}, %{freshness: :revalidated})
+
+    case :http_cache.cache(
+           http_cache_req,
+           to_http_cache_response(resp_env),
+           http_cache_revalidated_resp,
+           opts
+         ) do
+      {:ok, http_cache_resp} ->
+        to_tesla_response(resp_env, http_cache_resp)
+
+      :not_cacheable ->
+        {:ok, resp_env}
+    end
+  end
+
+  defp handle_response(
+         {:ok, %Tesla.Env{} = resp_env},
+         http_cache_req,
+         _http_cache_revalidated_resp,
+         _req_env,
+         opts
+       ) do
+    case :http_cache.cache(http_cache_req, to_http_cache_response(resp_env), opts) do
+      {:ok, http_cache_resp} ->
+        to_tesla_response(resp_env, http_cache_resp)
+
+      :not_cacheable ->
+        {:ok, resp_env}
+    end
+  end
+
+  defp handle_response(
+         {:error, reason} = error,
+         http_cache_req,
+         _http_cache_revalidated_resp,
+         req_env,
+         opts
+       ) do
+    if origin_unreachable?(reason) do
+      opts = Keyword.put(opts, :origin_unreachable, true)
+
+      case :http_cache.get(http_cache_req, opts) do
+        {:fresh, _} = http_cache_resp ->
+          return_cached_response(http_cache_resp, req_env, opts)
+
+        {:stale, _} = http_cache_resp ->
+          return_cached_response(http_cache_resp, req_env, opts)
+
+        _ ->
+          error
+      end
+    else
+      error
     end
   end
 
@@ -145,26 +191,6 @@ defmodule TeslaHTTPCache do
   rescue
     _ ->
       raise %__MODULE__.InvalidBodyError{}
-  end
-
-  defp cache_new_response(request, env, opts) do
-    case :http_cache.cache(request, to_http_cache_response(env), opts) do
-      {:ok, response} ->
-        to_tesla_response(env, response)
-
-      :not_cacheable ->
-        env
-    end
-  end
-
-  defp cache_revalidated_response(request, env, revalidated_response, opts) do
-    case :http_cache.cache(request, to_http_cache_response(env), revalidated_response, opts) do
-      {:ok, response} ->
-        to_tesla_response(env, response)
-
-      :not_cacheable ->
-        env
-    end
   end
 
   defp to_tesla_response(env, {status, resp_headers, {:sendfile, offset, :all, path}}) do
