278d9c58a8dc1510acd1dee5d84c49f4d790198b
Tangui <tangui.lepense@mail.ru>
2022-06-29 15:16:26 +0300

    Change minimal Elixir version to 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TeslaHTTPCache.MixProject do
     [
       app: :tesla_http_cache,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
